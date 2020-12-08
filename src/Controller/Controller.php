42ffbb74b2f9d0ee2ec03efcb24d2a06a888483d
SurvivalSlayer <nathan.lefebvre@live.fr>
2020-12-08 22:31:27 +0100

    package+controller

---
+++ src/Controller/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller; // Nous ajoutons le namespace
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class Controller extends AbstractController
+{
+
+    /**
+    * @Route("/Register", name="Register")
+    **/
+    
+}
